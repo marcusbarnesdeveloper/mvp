62cfc7394a57e4540a191bb480b1cb9afba0f7ec
root <root@marcus-HP.localdomain>
2020-06-08 18:56:14 -0700

    front end and api setup

--- server/index.js
+++ server/index.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const TOKEN = require('../utility');
 
 
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'));
+app.use(bodyParser.json())
 
 
-
+app.get('/api/recipes/:food', (req, res) =>{
+  const food = req.params.food;
+  axios.get('https://api.edamam.com/search',{
+    params: {
+      q: food,
+      app_id: TOKEN.id,
+      app_key: TOKEN.key
+    }
+  })
+    .then((results) => {
+      res.send(results.data.hits);
+      res.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      res.end();
+    })
+})
 app.listen(3001, () => {
   console.log('app on...');
 })
---
+++ utility.js
@@ -0,0 +1,7 @@
+const id = 'e79a4991';
+const key = '5bd759bd24b3c6f06f9dc6e740ae69c4';
+
+module.exports = {
+  id,
+  key
+}
